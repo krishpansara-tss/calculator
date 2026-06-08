3623c2c32aa8d469c0d00452dcc8ff5a795da3bd
Prince Kakadiya <prince.kakadiya@tssconsultancy.com>
2026-06-08 16:01:11 +0530

    code for addition and subtraction

--- calculator/src/com/calculator/Subtraction.java
+++ calculator/src/com/calculator/Subtraction.java
@@ -1,4 +1,8 @@
 package com.calculator;
 
 public class Subtraction {
+    public static void subNumber(int val1,int val2)
+    {
+        System.out.println(val1+" - "+val2+":" + (val1-val2));
+    }
 }
