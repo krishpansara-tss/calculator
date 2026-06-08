fb4d75b935f346cecfe4017ebf1e55b475a3f463
Hardik Kashiyani <hardik.kashiyani@tssconsultancy.com>
2026-06-08 15:58:30 +0530

    multiplication and division done

--- calculator/src/com/calculator/Division.java
+++ calculator/src/com/calculator/Division.java
@@ -1,4 +1,8 @@
 package com.calculator;
 
 public class Division {
+    public static int divideNumber(int number1,int number2)
+    {
+        return number1/number2;
+    }
 }
--- calculator/src/com/calculator/Multiplication.java
+++ calculator/src/com/calculator/Multiplication.java
@@ -1,4 +1,8 @@
 package com.calculator;
 
 public class Multiplication {
+    public static int multiplyNumber(int number1,int number2)
+    {
+        return number1*number2;
+    }
 }
